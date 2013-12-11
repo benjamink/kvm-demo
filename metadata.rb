afed45df4c246abade8dc79cb64a65d9fba0399c
Benjamin Krein <benk@aweber.com>
2013-12-10 22:42:18 -0500

    require 'apt' cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license           "none"
 description       "Setup virtualization TT demo"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1.0"
+
+depends           "apt"
